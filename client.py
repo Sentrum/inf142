6fd4ba05b7cf62753cb088d43a0efa05c1f74455
regn <regn@mail.com>
2021-05-24 14:47:00 +0200

    ip

--- client.py
+++ client.py
@@ -3,4 +3,4 @@ from socket import socket, AF_INET, SOCK_DGRAM
 sock = socket(AF_INET, SOCK_DGRAM)
 
 while(text := input("> ").lower()) != "shut down":
-    sock.sendto(text.encode(), ("localhost", 55551))
+    sock.sendto(text.encode(), ("81.191.115.9", 55551))
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from socket import socket, AF_INET, SOCK_DGRAM
 #Dgram to say its a udp
 
 sock = socket(AF_INET, SOCK_DGRAM)
-sock.bind(("localhost", 55551))
+sock.bind(("81.191.115.9", 55551))
 
 while True:
     msg, addr = sock.recvfrom(2048)
