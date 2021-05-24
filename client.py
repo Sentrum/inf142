b32bc4f62e8f867c48f59b74ca648c0e694dee46
Sentrum <81400286+Sentrum@users.noreply.github.com>
2021-05-24 17:12:02 +0200

    Delete client.py

--- client.py
+++
@@ -1,6 +0,0 @@
-from socket import socket, AF_INET, SOCK_DGRAM
-
-sock = socket(AF_INET, SOCK_DGRAM)
-
-while(text := input("> ").lower()) != "shut down":
-    sock.sendto(text.encode(), ("localhost", 55551))
